8858490227e1743cb1a0f6dd78e948105318c650
hoannl <hoans.email@gmail.com>
2026-04-11 16:49:07 +0700

    build 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.videomaker.aimusic"
         minSdk = 28
         targetSdk = 36
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
