9753d564ab21052a2867a7384d9ef5bd022f8eee
vic <vinguyen1801@gmail.com>
2026-05-13 13:29:22 +0700

    remove CTA to bottom language

--- app/src/main/java/com/videomaker/aimusic/modules/onboarding/pages/OnboardingPage4.kt
+++ app/src/main/java/com/videomaker/aimusic/modules/onboarding/pages/OnboardingPage4.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -97,7 +98,7 @@ fun FeatureSurveyPage(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .navigationBarsPadding()
+            .statusBarsPadding()
             .background(MaterialTheme.colorScheme.background)
             .verticalScroll(rememberScrollState())
             .padding(horizontal = 24.dp)
