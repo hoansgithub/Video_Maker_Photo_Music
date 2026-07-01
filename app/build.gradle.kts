5dd7c5c6c62dd0db324250f36aeb9ab75c92f191
vic <vinguyen1801@gmail.com>
2026-07-01 12:56:50 +0700

    up build 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 55
-        versionName = "1.1.2"
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
